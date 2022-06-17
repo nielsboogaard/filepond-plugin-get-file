fb9ccabf70a31ceb1c6e041b05827f40510ea5a8
Adrien Foulon <6115458+Tofandel@users.noreply.github.com>
2022-06-18 00:03:59 +0200

    Change download method

--- src/js/components/download.js
+++ src/js/components/download.js
@@ -6,14 +6,19 @@ export const registerDownloadComponent = (item, el, labelButtonDownload, allowDo
           downloadIcon = getDownloadIcon(labelButtonDownload);
 
     info.prepend(downloadIcon);
-    downloadIcon.addEventListener("click", () => downloadFile(item, allowDownloadByUrl));
+    if (!allowDownloadByUrl) {
+        downloadIcon.addEventListener("click", (e) => downloadFile(item, e));
+    }
 }
 
 /**
  * Generates the download icon
  */
-export const getDownloadIcon = (labelButtonDownload) => {
-    let icon = document.createElement('span');
+export const getDownloadIcon = (labelButtonDownload, item) => {
+    let icon = document.createElement('a');
+    icon.href = item.source;
+    icon.target = '_blank';
+    icon.rel = 'noopener';
     icon.className = 'filepond--download-icon';
     icon.title = labelButtonDownload;
     return icon;
@@ -22,21 +27,17 @@ export const getDownloadIcon = (labelButtonDownload) => {
 /**
  * Triggers the actual download of the uploaded file
  */
-export const downloadFile = (item, allowDownloadByUrl) => {
-    // if client want to download file from remote server
-    if(allowDownloadByUrl && item.getMetadata('url')) {
-        location.href = item.getMetadata('url'); // full path to remote server is stored in metadata with key 'url'
-      } else {
-        // create a temporary hyperlink to force the browser to download the file
-        const a = document.createElement("a");
-        const url = window.URL.createObjectURL(item.file);
-        document.body.appendChild(a);
-        a.style.display = 'none';
-        a.href = url;
-        a.download = item.file.name;
-        a.click();
+export const downloadFile = (item, e) => {
+    e.preventDefault();
+    // create a temporary hyperlink to force the browser to download the file
+    const a = document.createElement("a");
+    const url = window.URL.createObjectURL(item.file);
+    document.body.appendChild(a);
+    a.style.display = 'none';
+    a.href = url;
+    a.download = item.file.name;
+    a.click();
 
-        window.URL.revokeObjectURL(url);
-        a.remove();
-    }
+    window.URL.revokeObjectURL(url);
+    a.remove();
 }
